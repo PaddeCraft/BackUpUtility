PaddeCraft/BackUpUtility

commit 22da7bf484a521f602260dcd0cbb457ed660fa4e

Multiple changes, expand commit info to see everything - Switch to poetry and pyproject.toml - Add config version - Add automatic config version migration - Add configurable archive compression level

backup/__init__.py
@@ -1,1 +1,1 @@
-VERSION = "1.1.0"
+VERSION = "1.2.0"

backup/update.py
@@ -17,74 +17,74 @@ def update():
         sys.exit(1)
 
     py = sys.executable
-    downloadUrl = (
+    download_url = (
         f"https://github.com/PaddeCraft/BackUpUtility/archive/refs/tags/{ver}.zip"
     )
 
-    pkgsPipCmd = subprocess.run(
+    pkgs_pip_cmd = subprocess.run(
         [py, "-m", "pip", "list", "--format=freeze"], capture_output=True, text=True
     )
-    pkgsPipxCmd = subprocess.run(
+    pkgs_pipx_cmd = subprocess.run(
         [py, "-m", "pipx", "list", "--json"], capture_output=True, text=True
     )
-    if pkgsPipCmd.returncode != 0:
-        pkgsPipCmd = subprocess.run(
+    if pkgs_pip_cmd.returncode != 0:
+        pkgs_pip_cmd = subprocess.run(
             ["pip", "list", "--format=freeze"], capture_output=True, text=True
         )
-        if pkgsPipCmd.returncode != 0:
-            pkgsPipCmd = subprocess.run(
+        if pkgs_pip_cmd.returncode != 0:
+            pkgs_pip_cmd = subprocess.run(
                 ["pip3", "list", "--format=freeze"], capture_output=True, text=True
             )
-    if pkgsPipxCmd.returncode != 0:
-        pkgsPipxCmd = subprocess.run(
+    if pkgs_pipx_cmd.returncode != 0:
+        pkgs_pipx_cmd = subprocess.run(
             ["pipx", "list", "--json"], capture_output=True, text=True
         )
 
-    pkgsPip = []
-    pkgsPipx = []
+    pkgs_pip = []
+    pkgs_pipx = []
 
     print("Parsing current packages...")
 
-    for p in pkgsPipCmd.stdout.splitlines():
-        pkgsPip.append({"name": p.split("==")[0], "version": p.split("==")[1]})
+    for p in pkgs_pip_cmd.stdout.splitlines():
+        pkgs_pip.append({"name": p.split("==")[0], "version": p.split("==")[1]})
 
-    for _, pkg in json.loads(pkgsPipxCmd.stdout)["venvs"].items():
-        pkgsPipx.append(
+    for _, pkg in json.loads(pkgs_pipx_cmd.stdout)["venvs"].items():
+        pkgs_pipx.append(
             {
                 "name": pkg["metadata"]["main_package"]["package"],
                 "version": pkg["metadata"]["main_package"]["package_version"],
             }
         )
 
-    backUpPip = [p for p in pkgsPip if p["name"] == "BackUp"]
+    backup_pip = [p for p in pkgs_pip if p["name"] == "BackUp"]
 
-    if len(backUpPip) != 0:
-        if backUpPip[0]["version"] != ver:
+    if len(backup_pip) != 0:
+        if backup_pip[0]["version"] != ver:
             print("Updating BackUpUtility using pip...")
             p = subprocess.run(
-                [py, "-m", "pip", "install", downloadUrl], stdout=subprocess.DEVNULL
+                [py, "-m", "pip", "install", download_url], stdout=subprocess.DEVNULL
             )
             if p.returncode != 0:
                 p = subprocess.run(
-                    ["pip", "install", downloadUrl], stdout=subprocess.DEVNULL
+                    ["pip", "install", download_url], stdout=subprocess.DEVNULL
                 )
                 if p.returncode != 0:
                     p = subprocess.run(
-                        ["pip3", "install", downloadUrl], stdout=subprocess.DEVNULL
+                        ["pip3", "install", download_url], stdout=subprocess.DEVNULL
                     )
 
-    backUpPipx = [p for p in pkgsPipx if p["name"] == "BackUp"]
+    backup_pipx = [p for p in pkgs_pipx if p["name"] == "BackUp"]
 
-    if len(backUpPipx) != 0:
-        if backUpPipx[0]["version"] != ver:
+    if len(backup_pipx) != 0:
+        if backup_pipx[0]["version"] != ver:
             print("Updating BackUpUtility using pipx...")
             p = subprocess.run(
-                [py, "-m", "pipx", "install", downloadUrl, "--force"],
+                [py, "-m", "pipx", "install", download_url, "--force"],
                 stdout=subprocess.DEVNULL,
             )
             if p.returncode != 0:
                 p = subprocess.run(
-                    ["pipx", "install", downloadUrl, "--force"],
+                    ["pipx", "install", download_url, "--force"],
                     stdout=subprocess.DEVNULL,
                 )
 

pyproject.toml
@@ -1,0 +1,26 @@
+[tool.poetry]
+name = "backup"
+version = "1.2.0"
+description = "PaddeCraft's backup tool"
+authors = ["PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>"]
+license = "BSD-3-Clause"
+readme = "README.md"
+
+[tool.poetry.scripts]
+backup = "backup.__main__:app"
+
+[tool.poetry.dependencies]
+python = "^3.8"
+rich = "^13.5.2"
+toml = "^0.10.2"
+typer = "^0.9.0"
+humanize = "^4.8.0"
+requests = "^2.31.0"
+inquirerpy = "^0.3.4"
+
+[tool.poetry.group.dev.dependencies]
+black = "^23.7.0"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"

backup/__main__.py
@@ -10,7 +10,7 @@ from rich.progress import Progress
 from backup.config import *
 from backup.update import update as update_app
 from backup.log import configure, log
-from backup.layout import updateInfo, terminal, infoLayout, finishLayout
+from backup.layout import update_info, terminal, info_layout, finish_layout
 
 from backup.__init__ import VERSION
 
@@ -19,28 +19,24 @@ from InquirerPy import prompt
 
 app = Typer()
 
-totalFiles = 0
-totalSize = 0
-errorFiles = 0
+total_files = 0
+total_size = 0
+error_files = 0
 files = 0
 size = 0
 
-dayEndings = {1: "st", 2: "nd", 3: "rd", 21: "st", 22: "nd", 23: "rd", 31: "st"}
+
+def time_suffix(day):
+    # https://stackoverflow.com/a/5891598
+    return {1: "st", 2: "nd", 3: "rd"}.get(day % 20, "th")
 
 
-def timeSuffix(day):
-    try:
-        return dayEndings[day]
-    except KeyError:
-        return "th"
-
-
-def iterateFiles(pathList, excludeList):
-    for path in pathList:
+def iterate_files(path_list, exclude_list):
+    for path in path_list:
         for root, _, files in os.walk(path):
             for file in files:
                 include = True
-                for exclude in excludeList:
+                for exclude in exclude_list:
                     if exclude.replace("\\", "/") in os.path.join(root, file).replace(
                         "\\", "/"
                     ):
@@ -50,18 +46,18 @@ def iterateFiles(pathList, excludeList):
                         "src": os.path.join(root, file),
                         "relative": file,
                         "main": path,
-                        "mainLength": len(path),
+                        "main_length": len(path),
                     }
 
 
 @app.command()
 def backup(
-    configFileName: str = None,
+    config_file_name: str = None,
     create_config: bool = False,
     version: bool = False,
     update: bool = False,
 ):
-    global totalFiles, totalSize, files, size, errorFiles
+    global total_files, total_size, files, size, error_files
     if version:
         print("PaddeCraft's Backup Utility v" + VERSION)
         print("Copyright (C) PaddeCraft")
@@ -79,80 +75,80 @@ def backup(
         ]
         result = prompt(questions, style={"questionmark": "#ff9d00 bold"})
         filename = result[0].lower().replace(" ", "_")
-        createDefaultConfig(result[0], filename)
+        create_default_config(result[0], filename)
         print("Configuration created.")
         exit(0)
-    if configFileName is None:
-        cfgs = getConfigs()
-        if len(cfgs) == 0:
+    if config_file_name is None:
+        existing_configs = get_configs()
+        if len(existing_configs) == 0:
             print("No config files found.")
             print("You can create a new one with the --create-config flag.")
             exit(1)
-        if len(cfgs) == 1:
-            cfg = loadConfig(cfgs[0]["file"])
+        if len(existing_configs) == 1:
+            cfg = load_config(existing_configs[0]["file"])
         else:
             result = prompt(
                 [
                     {
                         "type": "list",
                         "message": "Select a backup configuration",
-                        "choices": [cfg["name"] for cfg in cfgs],
+                        "choices": [cfg["name"] for cfg in existing_configs],
                     }
                 ],
                 style={"questionmark": "#ff9d00 bold"},
             )
-            for c in cfgs:
+            for c in existing_configs:
                 if c["name"] == result[0]:
-                    cfg = loadConfig(c["file"])
+                    cfg = load_config(c["file"])
                     break
     else:
-        cfg = loadFromFileName(configFileName)
+        cfg = load_from_file_name(config_file_name)
 
-    t = datetime.utcfromtimestamp(cfg["lastBackupTime"])
+    t = datetime.utcfromtimestamp(cfg["last_backup_time"])
     print(
         "Fun fact: Your last backup with this configuration was started on "
         + t.strftime(
-            f'%A, the %-d{timeSuffix(int(t.strftime("%-d")))} %B %Y at %H:%M:%S.'
+            f'%A, the %-d{time_suffix(int(t.strftime("%-d")))} %B %Y at %H:%M:%S.'
         )
     )
-    time.sleep(8)
+    time.sleep(5)
 
     # Configure logging
     configure(cfg)
     with Progress(console=terminal) as progress:
         # Progress bar with no status for indicating the deleting
-        progress.log(infoLayout("Deleting last backup..."))
-        deletingBar = progress.add_task(
+        progress.log(info_layout("Deleting last backup..."))
+        deleting_bar = progress.add_task(
             "[bold]Deleting last backup [/bold] ", total=None
         )
 
         # Delete last backup if the user wants to
-        if cfg["lastBackup"] and cfg["lastBackupExists"] and cfg["deleteLast"]:
+        if cfg["last_backup"] and cfg["last_backup_exists"] and cfg["delete_last"]:
             try:
-                if os.path.isfile(cfg["lastBackup"]):
-                    os.remove(cfg["lastBackup"])
+                if os.path.isfile(cfg["last_backup"]):
+                    os.remove(cfg["last_backup"])
                 else:
-                    shutil.rmtree(cfg["lastBackup"])
+                    shutil.rmtree(cfg["last_backup"])
             except Exception as e:
                 log(f"Error deleting last backup: {str(e)}", error=True)
 
-        progress.update(deletingBar, visible=False)
-        progress.log(infoLayout("Indexing directories..."))
-        indexingBar = progress.add_task(
+        progress.update(deleting_bar, visible=False)
+        progress.log(info_layout("Indexing directories..."))
+        indexing_bar = progress.add_task(
             "[bold]Calculating space requirements [/bold] ", total=None
         )
 
         # Check total size and files
-        for f in iterateFiles(cfg["paths"], cfg["excludes"]):
+        for f in iterate_files(cfg["paths"], cfg["excludes"]):
             if os.access(f["src"], os.R_OK):
-                totalFiles += 1
-                totalSize += os.path.getsize(f["src"])
+                total_files += 1
+                total_size += os.path.getsize(f["src"])
             else:
-                errorFiles += 1
-        if totalSize > shutil.disk_usage(cfg["backupDest"])[2]:
+                error_files += 1
+        if total_size > shutil.disk_usage(cfg["backup_dest"])[2]:
             questions = [
                 {
-                    "message": "Not enough space aviable. Try anyways?",
+                    "message": "It is possible that the destination has less space available than needed. If you are using compression, the space may be sufficient. Try anyways?",
                     "type": "confirm",
                     "default": False,
                 },
@@ -165,72 +161,85 @@ def backup(
                 exit(0)
 
         # Create note that the last backup has been created
-        createLastBackupNote(cfg)
+        create_last_backup_note(cfg)
 
         # Progress bar for indicating the backup phase
-        progress.update(indexingBar, visible=False)
-        backupBar = progress.add_task("[bold]Backing files up [/bold]", total=totalSize)
+        progress.update(indexing_bar, visible=False)
+        backupBar = progress.add_task(
+            "[bold]Backing files up [/bold]", total=total_size
+        )
 
         # Open the zip file
-        if cfg["useZip"]:
+        if cfg["use_zip"]:
+            comp_level = cfg["zip_compression_level"]
+            if comp_level == 0:
+                comp_type = zipfile.ZIP_STORED
+            elif comp_level in range(1, 10):  # 1 <= level <= 9
+                comp_type = zipfile.ZIP_BZIP2
+            else:
+                comp_type = zipfile.ZIP_LZMA
+
             zip = zipfile.ZipFile(
-                cfg["backupPath"],
+                cfg["backup_path"],
                 "a",
                 strict_timestamps=False,
-                compression=zipfile.ZIP_DEFLATED,
+                compression=comp_type,
+                compresslevel=comp_level,
             )
 
         # The backup phase
-        for f in iterateFiles(cfg["paths"], cfg["excludes"]):
-            if os.access(f["src"], os.R_OK):
-                files += 1
-                sz = os.path.getsize(f["src"])
-                size += sz
-                progress.log(updateInfo(f["src"], totalFiles, totalSize, files, size))
-                try:
-                    if cfg["useZip"]:
-                        zip.write(f["src"], f["src"][f["mainLength"] :])
-                    else:
-                        relPth = "/".join(
-                            f["src"]
-                            .replace(f["main"], "")
-                            .replace("\\", "/")
-                            .split("/")[:-1]
-                        )
-                        try:
-                            if relPth[0] == "/":
-                                relPth = relPth[1:]
-                        except:
-                            pass
-                        mainDir = f["main"]
-                        if mainDir[-1] == "/":
-                            mainDir = mainDir[:-1]
-                        mainDir = mainDir.replace("\\", "/").split("/")[-1]
-                        folder = os.path.join(
-                            cfg["backupDest"],
-                            cfg["backupName"],
-                            mainDir,
-                            relPth,
-                        )
-                        os.makedirs(folder, exist_ok=True)
-                        shutil.copy2(
-                            f["src"], os.path.join(folder, f["src"].split("/")[-1])
-                        )
-                    log(f"Added {f['src']} to backup.")
-                except Exception as e:
-                    log(f"Error while backing up {f['src']}: {str(e)}", error=True)
-                progress.update(backupBar, advance=sz)
-            else:
+        for f in iterate_files(cfg["paths"], cfg["excludes"]):
+            if not os.access(f["src"], os.R_OK):
                 log(f"Error: {f['src']} is not readable.", error=True)
+                continue
+
+            files += 1
+            sz = os.path.getsize(f["src"])
+            size += sz
+            progress.log(update_info(f["src"], total_files, total_size, files, size))
+            try:
+                if cfg["use_zip"]:
+                    zip.write(f["src"], f["src"][f["main_length"] :])
+                else:
+                    rel_pth = "/".join(
+                        f["src"]
+                        .replace(f["main"], "")
+                        .replace("\\", "/")
+                        .split("/")[:-1]
+                    )
+                    try:
+                        if rel_pth[0] == "/":
+                            rel_pth = rel_pth[1:]
+                    except:
+                        pass
+                    main_dir = f["main"]
+                    if main_dir[-1] == "/":
+                        main_dir = main_dir[:-1]
+                    main_dir = main_dir.replace("\\", "/").split("/")[-1]
+                    folder = os.path.join(
+                        cfg["backup_dest"],
+                        cfg["backup_name"],
+                        main_dir,
+                        rel_pth,
+                    )
+                    os.makedirs(folder, exist_ok=True)
+                    shutil.copy2(
+                        f["src"], os.path.join(folder, f["src"].split("/")[-1])
+                    )
+                log(f"Added {f['src']} to backup.")
+            except Exception as e:
+                log(f"Error while backing up {f['src']}: {str(e)}", error=True)
+                error_files += 1
+            progress.update(backupBar, advance=sz)
 
         # Close the zip file if it was used
-        if cfg["useZip"]:
+        if cfg["use_zip"]:
             zip.close()
-        progress.log(finishLayout(errorFiles, files, size, cfg))
+        progress.log(finish_layout(error_files, files, size, cfg))
         log(
             f"""\n
           Backup finished.
-          {errorFiles} files could not be backed up.
+          {error_files} files could not be backed up.
           {files} files were backed up.
           {size} bytes were backed up."""
         )

backup/config.py
@@ -3,89 +3,94 @@ import toml
 import time
 from datetime import datetime
 
-defaultConfig = {
-    'name': 'Example config',
-    'paths': [
-        '/some/example/path',
-        '/some/other/path'
-    ],
-    'excludes': [
-        '*.tmp',
-        '/some/example/path/to/exclude'
-    ],
-    'settings': {
-        'useZip': True,
-        'deleteLast': True,
-        'backupDest': '/run/media/someuser/somedisk/backups',
-        'backupName': 'backup-%d-%m-%Y-%H-%M-%S',
-    }, 'doNotTouch': {
-        'lastBackup': False,
-        'lastBackupName': '',
-        'lastBackupTime': 0,
-    }
+from .config_migrations import migrate_config
+
+default_config = {
+    "name": "Example config",
+    "version": 1,
+    "paths": ["/some/example/path", "/some/other/path"],
+    "excludes": ["*.tmp", "/some/example/path/to/exclude"],
+    "settings": {
+        "use_zip": True,
+        "zip_compression_level": 0,
+        "delete_last": True,
+        "backup_dest": "/run/media/someuser/somedisk/backups",
+        "backup_name": "backup-%d-%m-%Y-%H-%M-%S",
+    },
+    "do_not_touch": {
+        "last_backup": False,
+        "last_backup_name": "",
+        "last_backup_time": 0,
+    },
 }
 
-userPath = os.path.expanduser("~")
+home_path = os.path.expanduser("~")
 
-softwarePath = os.path.join(
-    userPath, ".PaddeCraftSoftware", "BackUp")
-configPath = os.path.join(softwarePath, "config")
-logPath = os.path.join(softwarePath, "logs")
+software_path = os.path.join(home_path, ".PaddeCraftSoftware", "BackUp")
+config_path = os.path.join(software_path, "config")
+log_path = os.path.join(software_path, "logs")
 
-os.makedirs(logPath, exist_ok=True)
-os.makedirs(configPath, exist_ok=True)
+os.makedirs(log_path, exist_ok=True)
+os.makedirs(config_path, exist_ok=True)
 
 
-def createDefaultConfig(name, fname):
-    with open(os.path.join(configPath, fname + ".toml"), 'w+', encoding="utf-8") as conf:
-        c = defaultConfig
-        c['name'] = name
+def create_default_config(name, fname):
+    with open(
+        os.path.join(config_path, fname + ".toml"), "w+", encoding="utf-8"
+    ) as conf:
+        c = default_config
+        c["name"] = name
         toml.dump(c, conf)
 
 
-def getConfigs():
+def get_configs():
     configs = []
-    for file in os.listdir(configPath):
+    for file in os.listdir(config_path):
         if file.endswith(".toml"):
-            f = os.path.join(configPath, file)
+            f = os.path.join(config_path, file)
             content = toml.load(f)
-            name = content['name']
-            configs.append({'name': name, 'file': f})
+            name = content["name"]
+            configs.append({"name": name, "file": f})
     return configs
 
 
-def loadConfig(file):
+def load_config(file):
+    migrate_config(file)
+
     content = toml.load(file)
     now = datetime.now()
-    useZip = content['settings']['useZip']
-    dest = content['settings']['backupDest']
-    name = now.strftime(content['settings']
-                        ['backupName']) + (".zip" if useZip else "")
+    use_zip = content["settings"]["use_zip"]
+    dest = content["settings"]["backup_dest"]
+    name = now.strftime(content["settings"]["backup_name"]) + (
+        ".zip" if use_zip else ""
+    )
+
     return {
-        'paths': content['paths'],
-        'excludes': content['excludes'],
-        'useZip': useZip,
-        'deleteLast': content['settings']['deleteLast'],
-        'backupDest': dest,
-        'backupName': name,
-        'backupPath': os.path.join(dest, name),
-        'startTime': now,
-        'logPath': os.path.join(logPath, name + ".log"),
-        'configFile': file,
-        'lastBackup': content['doNotTouch']['lastBackupName'],
-        'lastBackupExists': content['doNotTouch']['lastBackup'],
-        'lastBackupTime': content['doNotTouch']['lastBackupTime']
+        "paths": content["paths"],
+        "excludes": content["excludes"],
+        "use_zip": use_zip,
+        "zip_compression_level": content["settings"]["zip_compression_level"],
+        "delete_last": content["settings"]["delete_last"],
+        "backup_dest": dest,
+        "backup_name": name,
+        "backup_path": os.path.join(dest, name),
+        "start_time": now,
+        "log_path": os.path.join(log_path, name + ".log"),
+        "config_file": file,
+        "last_backup": content["do_not_touch"]["last_backup_name"],
+        "last_backup_exists": content["do_not_touch"]["last_backup"],
+        "last_backup_time": content["do_not_touch"]["last_backup_time"],
     }
 
 
-def loadFromFileName(fileName):
-    return loadConfig(os.path.join(configPath, fileName))
+def load_from_file_name(fname):
+    return load_config(os.path.join(config_path, fname))
 
 
-def createLastBackupNote(cfg):
-    content = toml.load(cfg["configFile"])
-    with open(cfg["configFile"], 'w', encoding="utf-8") as f:
-        content['doNotTouch']['lastBackup'] = True
-        content['doNotTouch']['lastBackupName'] = cfg['backupPath']
-        content['doNotTouch']['lastBackupTime'] = int(time.time())
+def create_last_backup_note(cfg):
+    content = toml.load(cfg["config_file"])
+    with open(cfg["config_file"], "w", encoding="utf-8") as f:
+        content["do_not_touch"]["last_backup"] = True
+        content["do_not_touch"]["last_backup_name"] = cfg["backup_path"]
+        content["do_not_touch"]["last_backup_time"] = int(time.time())
         toml.dump(content, f)

backup/config_migrations.py
@@ -1,0 +1,114 @@
+CONFIG_VERSION_MIGRATIONS = [
+    {
+        "version": 1,  # Version is the version of the config after migration
+        "actions": [
+            # Settings
+            {"action": "rename", "from": "settings.useZip", "to": "settings.use_zip"},
+            {
+                "action": "rename",
+                "from": "settings.deleteLast",
+                "to": "settings.delete_last",
+            },
+            {
+                "action": "rename",
+                "from": "settings.backupDest",
+                "to": "settings.backup_dest",
+            },
+            {
+                "action": "rename",
+                "from": "settings.backupName",
+                "to": "settings.backup_name",
+            },
+            {"action": "create", "key": "settings.zip_compression_level", "value": 0},
+            # Do not touch
+            {"action": "rename", "from": "doNotTouch", "to": "do_not_touch"},
+            {
+                "action": "rename",
+                "from": "do_not_touch.lastBackup",
+                "to": "do_not_touch.last_backup",
+            },
+            {
+                "action": "rename",
+                "from": "do_not_touch.lastBackupName",
+                "to": "do_not_touch.last_backup_name",
+            },
+            {
+                "action": "rename",
+                "from": "do_not_touch.lastBackupTime",
+                "to": "do_not_touch.last_backup_time",
+            },
+        ],
+    }
+]
+
+import toml
+
+
+def resolve_parent_key(config, path):
+    paths = path.split(".")
+    final_key = paths[-1]
+    paths.pop(-1)
+
+    data = config
+    for pth in paths:
+        if not pth in data:
+            data[pth] = {}
+        data = data[pth]
+
+    return data, final_key
+
+
+def pop_key(config, path):
+    data, final_key = resolve_parent_key(config, path)
+
+    ret = data[final_key]
+    del data[final_key]
+    return ret
+
+
+def insert_key(config, path, data):
+    _data, final_key = resolve_parent_key(config, path)
+    _data[final_key] = data
+
+
+def migrate_config(fname):
+    config = toml.load(fname)
+    version = 0
+    if "version" in config:
+        version = config["version"]
+
+    latest_version = CONFIG_VERSION_MIGRATIONS[-1]["version"]
+
+    if version == latest_version:
+        return
+
+    migration_count = 0
+    migration_log = ""
+    for migration in CONFIG_VERSION_MIGRATIONS:
+        if not migration["version"] > version:
+            continue
+
+        for action in migration["actions"]:
+            match action["action"]:
+                case "rename":
+                    data = pop_key(config, action["from"])
+                    insert_key(config, action["to"], data)
+
+                case "create":
+                    insert_key(config, action["key"], action["value"])
+
+            migration_log += (
+                " ".join([f"{k.upper()}={v}" for k, v in action.items()]) + "\n"
+            )
+
+        migration_count += 1
+
+    config["version"] = latest_version
+    print(
+        f"Your configuration was {migration_count} version{'s' if migration_count > 1 else ''} behind ({version}=>{latest_version}). It was automatically migrated."
+    )
+    print("Migration log:")
+    print(migration_log, end="\n\n")
+
+    with open(fname, "w", encoding="utf-8") as f:
+        toml.dump(config, f)

backup/layout.py
@@ -2,6 +2,7 @@ from rich.console import Console
 from rich.layout import Layout
 from rich.panel import Panel
 
+from textwrap import dedent
 from humanize import naturalsize, intcomma, naturaldelta
 
 from datetime import datetime
@@ -22,31 +23,35 @@ layout["spacer"].size = 1
 layout["spacer2"].size = 2
 
 
-def updateInfo(path, totalFiles, totalSize, files, size):
+def update_info(path, total_files, total_size, files, size):
     layout["info"].update(
-        f"""\
-    [bold]Path:[/bold] {path}
-    [bold]Total files:[/bold] {intcomma(files)}/{intcomma(totalFiles)}
-    [bold]Total size:[/bold] {naturalsize(size)}/{naturalsize(totalSize)}"""
+        dedent(
+            f"""\
+            [bold]Path:[/bold] {path}
+            [bold]Total files:[/bold] {intcomma(files)}/{intcomma(total_files)}
+            [bold]Total size:[/bold] {naturalsize(size)}/{naturalsize(total_size)}"""
+        )
     )
     return layout
 
 
-def infoLayout(text):
+def info_layout(text):
     layout["info"].update("[bold]" + text + "[/bold]")
     return layout
 
 
-def finishLayout(errors, files, size, cfg):
+def finish_layout(errors, files, size, cfg):
     layout["info"].update(
-        f"""\
-        [bold]Backup finished![/bold]
-        
-        [bold]Total files:[/bold] {intcomma(files + errors)}
-        [bold]Unsucsessfull files:[/bold] {intcomma(errors)}
-        [bold]Total size:[/bold] {naturalsize(size)}
-        [bold]Time taken:[/bold] {naturaldelta(datetime.now() - cfg['startTime'])}
-        
-        A complete log has been written to {cfg['logPath']}."""
+        dedent(
+            f"""\
+            [bold]Backup finished![/bold]
+
+            [bold]Total files:[/bold] {intcomma(files + errors)}
+            [bold]Unsucsessfull files:[/bold] {intcomma(errors)}
+            [bold]Total size:[/bold] {naturalsize(size)}
+            [bold]Time taken:[/bold] {naturaldelta(datetime.now() - cfg['start_time'])}
+
+            A complete log has been written to {cfg['log_path']}."""
+        )
     )
     return layout

backup/log.py
@@ -2,7 +2,7 @@ import logging
 
 
 def configure(cfg):
-    logging.basicConfig(filename=cfg["logPath"], level=logging.DEBUG)
+    logging.basicConfig(filename=cfg["log_path"], level=logging.DEBUG)
 
 
 def log(message, error=False):

commit 1b915853a09a951cb2c4392da6c0894c8fbbf80a

Fix updater, broken since commit 22da7bf

backup/__init__.py
@@ -1,1 +1,1 @@
-VERSION = "1.3.0"
+VERSION = "1.3.1"

backup/update.py
@@ -56,7 +56,7 @@ def update():
             }
         )
 
-    backup_pip = [p for p in pkgs_pip if p["name"] == "BackUp"]
+    backup_pip = [p for p in pkgs_pip if p["name"] == "backup"]
 
     if len(backup_pip) != 0:
         if backup_pip[0]["version"] != ver:
@@ -73,7 +73,7 @@ def update():
                         ["pip3", "install", download_url], stdout=subprocess.DEVNULL
                     )
 
-    backup_pipx = [p for p in pkgs_pipx if p["name"] == "BackUp"]
+    backup_pipx = [p for p in pkgs_pipx if p["name"] == "backup"]
 
     if len(backup_pipx) != 0:
         if backup_pipx[0]["version"] != ver:

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "backup"
-version = "1.3.0"
+version = "1.3.1"
 description = "PaddeCraft's backup tool"
 authors = ["PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>"]
 license = "BSD-3-Clause"